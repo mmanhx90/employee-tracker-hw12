2cd13e50887fb97e57dfef0d895df04c1c1c1fe1
MICHAEL MANHXAYTHAVONG <mmanhxaythavong@gmail.com>
2023-06-09 13:00:16 -0700

    homework 12

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employees_db
+CREATE DATABASE employees_db
+USE employees_db
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id),
+    REFERENCES department(id),
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id),
+    REFERENCES department(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id),
+    REFERENCES department(id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (department_name)
+VALUES  ("Engineering"),
+        ("Finance"),
+        ("Legal"),
+        ("Sales");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Engineer", 1500000, 1)
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES  ("Michael", "Manhxaythavong", 1, 1)
